4496c97f4bf65743c86a4f0d40b805372af0db73
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-08-04 16:02:56 -0300

    Create SaleService.java

---
+++ BackEnd/src/main/java/com/VictorMesquita/dsmeta/services/SaleService.java
@@ -0,0 +1,32 @@
+package com.VictorMesquita.dsmeta.services;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.VictorMesquita.dsmeta.entities.Sale;
+import com.VictorMesquita.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		//data de hoje do usuario
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+				
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		
+		return repository.findSales(min, max, pageable);
+	}
+}
